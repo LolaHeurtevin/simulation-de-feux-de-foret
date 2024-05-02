64a50ac07079b59d4cd7c60c91702650d3f86a23
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-02 11:52:58 +0200

    Ajout des fonctions ppur séléctionner le type de terrain et de climat pour la simulation : success

--- setSimulation.dart
+++ setSimulation.dart
@@ -40,3 +40,72 @@ List<Cell> setBurningCell(List<Cell> cellList, List<int> startingCells) {
 
   return cellList;
 }
+
+//// Initialisation du terrain et climat /////
+
+enum Climat { humid, normal, dry, veryDry }
+
+enum Terrain { continu, close, spaced, sparse }
+
+class InitialisationSimulation {
+  Climat climat;
+  Terrain terrain;
+
+  InitialisationSimulation(this.climat, this.terrain);
+}
+
+Climat choisirClimat() {
+  print('Choisissez le type de climat :');
+  print('1. Humide');
+  print('2. Normal');
+  print('3. Sec');
+  print('4. Très sec');
+  
+  stdout.write('Entrez le numéro correspondant au type de climat : ');
+  int choix = int.parse(stdin.readLineSync()!);
+
+  switch (choix) {
+    case 1:
+      return Climat.humid;
+    case 2:
+      return Climat.normal;
+    case 3:
+      return Climat.dry;
+    case 4:
+      return Climat.veryDry;
+    default:
+      print('Choix invalide, sélection par défaut : Normal');
+      return Climat.normal;
+  }
+}
+
+Terrain choisirTerrain() {
+  print('Choisissez le type de terrain :');
+  print('1. Continue');
+  print('2. Peu espacée');
+  print('3. Espacée');
+  print('4. Clairsemé');
+  
+  stdout.write('Entrez le numéro correspondant au type de terrain : ');
+  int choix = int.parse(stdin.readLineSync()!);
+
+  switch (choix) {
+    case 1:
+      return Terrain.continu;
+    case 2:
+      return Terrain.close;
+    case 3:
+      return Terrain.spaced;
+    case 4:
+      return Terrain.sparse;
+    default:
+      print('Choix invalide, sélection par défaut : Continue');
+      return Terrain.continu;
+  }
+}
+
+InitialisationSimulation initialiserSimulation() {
+  Climat climatChoisi = choisirClimat();
+  Terrain terrainChoisi = choisirTerrain();
+  return InitialisationSimulation(climatChoisi, terrainChoisi);
+}

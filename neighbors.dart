2b445a971be3c38e1bd26c500f91a414264cdd16
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-02 12:03:00 +0200

    Cleanup du code

--- neighbors.dart
+++ neighbors.dart
@@ -1,7 +1,7 @@
 // Fonction pour obtenir les indices des cellules voisines d'une cellule donnée
 List<int> neighbors(int index, int total) {
-  List<int> results = [];  // Liste pour stocker les indices des voisins
-  if (index > 0) results.add(index - 1);  // Ajoute le voisin de gauche si ce n'est pas la première cellule
-  if (index < total - 1) results.add(index + 1);  // Ajoute le voisin de droite si ce n'est pas la dernière cellule
-  return results;  // Retourne la liste des voisins
+  List<int> results = [];  
+  if (index > 0) results.add(index - 1);  
+  if (index < total - 1) results.add(index + 1); 
+  return results; 
 }

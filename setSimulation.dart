e2bba537c957b2d56ddd11a181322ce1efd058eb
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-02 11:16:45 +0200

    fonction chooseBurningCell fonction et permet de choisir 2 indexe de cellules à enflammer

--- setSimulation.dart
+++ setSimulation.dart
@@ -1,23 +1,28 @@
-// fonction qui détermine quelles cases sont enflammées au début de la simulation
 import 'dart:io';
+import './cell.dart';
 
-List<int> setSimulation(int total) {
-  List<int> results = [];  // Liste pour stocker les indices des cellules
-  stdout.write('Veuillez saisir l\'index de la première cellule enflammée : ');
+// fonction qui détermine quelles cases sont enflammées au début de la simulation
+List<int> chooseBurningCells(int total) {
+  List<int> results = [];  // Liste pour stocker les indices des cellules enfalmmées
 
-  String? userInput = stdin.readLineSync();
+  for (int i=0; i<=1; i++) {
+    stdout.write('Veuillez saisir l\'index de la cellule à enflammer : ');
 
-  if (userInput != null) {
-    int? cellIndex = int.tryParse(userInput); 
-    
-    if (cellIndex != null) {
-      print('Vous avez saisi l\'index de la cellule : $cellIndex');
+    String? userInput = stdin.readLineSync();
+
+    if (userInput != null) {
+      int? cellIndex = int.tryParse(userInput); 
+
+      if (cellIndex != null) {
+        results.add(cellIndex);
+      } else {
+        print('Veuillez saisir un index de cellule valide (un entier).');
+      }
     } else {
-      print('Veuillez saisir un index de cellule valide (un entier).');
+      print('Aucune entrée fournie.');
     }
-  } else {
-    print('Aucune entrée fournie.');
   }
 
   return results; 
 }
+

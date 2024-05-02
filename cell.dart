030a40ca30c90f307f2ce059399b4f40a3763c7a
MAROUA <dihabi.maroua1@gmail.com>
2024-05-02 10:30:30 +0200

    afficher l'état d'une cellule à partir de son indice 'index'

--- cell.dart
+++ cell.dart
@@ -21,7 +21,7 @@ void main() {
 
   // Définir l'indice de la cellule que nous voulons examiner
   int index = 2;
-
+  
   // Affiche l'état de la cellule à l'indice spécifié
   print('État de la cellule $index : ${find(list, index).state}');
 }

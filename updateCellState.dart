cc9dd09454398a7da75286d81afb92cb55ecb6c3
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-21 21:54:01 +0200

    Création de la fonction cellTurnsCold qui détermine si une cellule passe à l'état brpulé et froid et mise à jour de la fonction updateCellState pour permettre ce changement

--- updateCellState.dart
+++ updateCellState.dart
@@ -1,33 +1,50 @@
 import 'dart:math';
 import './cell.dart';
 import 'setSimulation.dart';
+import './randomNumber.dart';
 
 enum Climat { humid, normal, dry, veryDry }
 
 
 // Fonction pour mettre à jour l'état d'une cellule spécifique
 void updateCellState(Cell cell, int round) {
-  if (round >= 3 && cell.state == 'enflammée') {
-    cell.state = 'brûlé et chaud';  // Change l'état à 'brûlé et chaud'
+  if (cell.state == 'enflammée' && cell.lastUpdated >= 2) {
+    cell.state = 'brûlé et chaud'; 
+    cell.lastUpdated = 0;
   }
+
+  if (cell.state == 'brûlé et chaud' && cell.lastUpdated >=1 && cellTurnsCold() == true) {
+    cell.state = 'brûlé et froid';
+    cell.lastUpdated = 0;
+  }
+
+  cell.lastUpdated +=1;
 }
 
 // fonction qui permet le changement ou non d'état d'une cellule après qu'elle ai reçu un brandon
 // la cellule ayant reçu le brandon a différentes chances de s'enflammer en fonction du climat choisi
 bool willInflammeAfterReceivingBrandon (Climat climat) {
-  Random random = Random();
-  double randomNumber = (random.nextDouble() * 10).toInt() / 10;
 
   switch (climat) {
     case Climat.humid:
-      return randomNumber < 0.1;
+      return randomNumber(0, 100) <= 10;
     case Climat.normal:
-      return randomNumber < 0.3;
+      return randomNumber(0, 100) <= 30;
     case Climat.dry:
-      return randomNumber < 0.6;
+      return randomNumber(0, 100) <= 60;
     case Climat.veryDry:
-      return randomNumber < 0.9;
+      return randomNumber(0, 100) <= 90;
     default:
       throw ArgumentError('Climat non valide');
   }
+}
+
+/**
+ * fonction cellTurnsCold
+ * détermine si une cellule passe dans l'état brulé et froid
+ * @param
+ * @return bool
+ */
+bool cellTurnsCold() {
+  return randomNumber(0, 100) <= 40;
 }

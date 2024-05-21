f9e5344241cf0c52f6cffc756b9ee5edc3d59e67
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-21 21:55:28 +0200

    Création de l'attribut lastUpdated qui permet de savoir depuis combien de rounds la cellule n'a pas été mise à jour

--- cell.dart
+++ cell.dart
@@ -1,6 +1,7 @@
 // la classe Cell représente une cellule
 class Cell {
   String state; // stocker l'etat d'une cellule
+  int lastUpdated = 0;
   Cell(this.state); // constructeur d'initialisation
 }
 
